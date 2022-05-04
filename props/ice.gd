25d9bf56721b351be666bcf64d48337331b0fafd
Eric <eric@pedr0.net>
2022-05-04 23:38:11 +0200

    Create some new items

---
+++ props/ice.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+
+func _on_Area2D_body_entered(body):
+	if body.is_in_group("player"):
+		$AnimationPlayer.play("down")
+
+
+func _on_Area2D_body_exited(body):
+	$AnimationPlayer.play_backwards("down")
